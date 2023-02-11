e83d2b7676e60122f76e604dd9901a362cdbde4e
Bruno <brunorodriguesdias@gmail.com>
2023-02-11 11:48:30 -0300

    Começando a criar a classe Manipulação de objetos

--- src/AcervoDaBiblioteca.java
+++ src/AcervoDaBiblioteca.java
@@ -1,16 +1,16 @@
 import java.util.List;
 public abstract class AcervoDaBiblioteca {
-    private List<Usuarios> listaUsuarios;
+//    private List<Usuarios> listaUsuarios;
 
-    private List<Livro> livrosFisicos;
-    private List<Livro> ebooks;
+//    private List<Livro> livrosFisicos;
+//    private List<Livro> ebooks;
 
-    public void cadastrarLivro(Livro livro){
-        if(livro.getTipo().equals("1")){
-            livrosFisicos.add(livro);
-        } else {
-            ebooks.add(livro);
-        }
-    }
+//    public void cadastrarLivro(Livro livro){
+//        if(livro.getTipo().equals("1")){
+//            livrosFisicos.add(livro);
+//        } else {
+//            ebooks.add(livro);
+//        }
+//    }
 
 }
--- src/ManipulacaoDeObjetos.java
+++ src/ManipulacaoDeObjetos.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class ManipulacaoDeObjetos {
+import java.util.ArrayList;
+import java.util.List;
+public class ManipulacaoDeObjetos {
+    private List<Livro> livrosFisicos;
+    private List<Livro> ebooks;
+    private List<Usuarios> listaDeUsuarios;
+    private List<Aluguel> emprestimos;
+
+    public ManipulacaoDeObjetos() {
+        this.livrosFisicos = new ArrayList<>();
+        this.ebooks = new ArrayList<>();
+        this.listaDeUsuarios = new ArrayList<>();
+        this.emprestimos = new ArrayList<>();
+    }
+    //Comando de adicionar objetos nas listas
+    public void adicionarUsuario(Usuarios usuario) {
+        this.listaDeUsuarios.add(usuario);
+    }
+
+    public void adicionarLivroFisico(Livro livroFisico) {
+        this.livrosFisicos.add(livroFisico);
+    }
+
+    public void adicionarEbook(Livro ebook) {
+        this.ebooks.add(ebook);
+    }
+
+    public void adicionarUsuario(Aluguel emprestimo) {
+        this.emprestimos.add(emprestimo);
+    }
+
+    //Comando de remover objetos
+    public void removerLivroFisicoPorId(String idLivro){
+        this.livrosFisicos.remove(idLivro);
+    }
+    public void removerEbook(String ebook){
+        this.livrosFisicos.remove(ebook);
+    }
+    public void removerUsuarioPorIndice(String indice){
+        this.listaDeUsuarios.remove(indice);
+    }
+    public void removerEmprestimo(int idEmprestimo){
+        this.emprestimos.remove(emprestimos);
+    }
 }
+
